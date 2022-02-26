e82c2535289298448714f5aa0d713dd7b1289419
MakWils <kaylaanngrace@gmail.comgit remote add origin git@github.com:kaylaanngrace/horiseon-optimize-accessibility.git>
2022-02-26 00:57:20 -0600

    installed inquirer

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [userName, github] = profileDataArgs;
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-fs.writeFile('./index.html', generatePage(userName,github), err => {
-    if (err) throw new Error(err);
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+// const pageHTML = generatePage(userName, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw new Error(err);
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
